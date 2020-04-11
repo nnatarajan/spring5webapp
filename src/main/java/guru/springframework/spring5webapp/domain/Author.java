744c4ce6a74a18ed7d79ddc2bcbd47524a3dfb47
Natasha <20606687+nnatarajan@users.noreply.github.com>
2020-04-11 10:35:52 -0600

    repositories, start to initialize data

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -15,15 +15,15 @@ public class Author{
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new ;
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
+
     }
 
     public Long getId() {
